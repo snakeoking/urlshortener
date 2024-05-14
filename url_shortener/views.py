6076985f1941d19f4b9be96a220564b674967826
akashbhaskar2011 <akashbhaskar.2011@gmail.com>
2024-05-14 14:53:07 +0530

    final changes

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -10,6 +10,14 @@ def index(request):
 def shorten_url(request):
     if request.method == 'POST':
         orignal_url = request.POST.get('orignal_url')
+        
+        # Check if the orignal URL already exists in the database
+        existing_url = URL.objects.filter(orignal_url=orignal_url).first()
+        if existing_url:
+            short_url = request.build_absolute_uri('/') + existing_url.short_url
+            return render(request, 'index.html', {'short_url': short_url})
+        
+        # If the orignal URL doesn't exist, generate a short code and create a new entry
         short_code = generate_short_code()
         short_url = request.build_absolute_uri('/') + short_code
         URL.objects.create(orignal_url=orignal_url, short_url=short_code)
