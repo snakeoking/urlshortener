48398cf1bb1ed59cbe018c2516d67e639b08b0b6
akashbhaskar2011 <akashbhaskar.2011@gmail.com>
2024-05-14 12:53:48 +0530

    complete working project;

---
+++ url_shortener/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import URL
+import random
+import string
+
+def index(request):
+    return render(request, 'index.html')
+
+def shorten_url(request):
+    if request.method == 'POST':
+        orignal_url = request.POST.get('orignal_url')
+        short_code = generate_short_code()
+        short_url = request.build_absolute_uri('/') + short_code
+        URL.objects.create(orignal_url=orignal_url, short_url=short_code)
+        return render(request, 'index.html', {'short_url': short_url})
+    return redirect('index')
+
+def redirect_original_url(request, short_url):
+    orignal_url = URL.objects.get(short_url=short_url).orignal_url
+    return redirect(orignal_url)
+
+def generate_short_code():
+    characters = string.ascii_letters + string.digits
+    short_code = ''.join(random.choice(characters) for i in range(6))
+    while URL.objects.filter(short_url=short_code).exists():
+        short_code = ''.join(random.choice(characters) for i in range(6))
+    return short_code
